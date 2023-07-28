f648f952de1d2bbc43b5357dfb060f91e8b980a8
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-28 15:10:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // Initial position of the array.
+  for (let i=0;i<array.length;i++){
+    // Compare the position with the rest of the positions
+    for (let j=i+1;j<array.length;j++){
+      if(array[i] + array[j] === target){ //return true
+        return true
+      }
+    } 
+  }
+  return false
+
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Steps: 2, Big ) time Complexity is O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Grab an initial position of the array, and another position that is not the initial position.
+  Compare each item of the array to the others.
+    If the two add up to the target, return true.
+    Else return false.
 */
 
 /*
   Add written explanation of your solution here
+The function takes two parameters, an array and a target number
+Return true if the any two numbers in the array add up to target.
+Or else return false.
+
+
 */
 
 // You can run `node index.js` to view these console logs
